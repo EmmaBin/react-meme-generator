5ac671703203e4b4000ba4967854ac9c4617faca
emmacodes <binma.emma@gmail.com>
2023-01-09 15:32:41 -0800

    added header component and styled

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Header(){
+    return(
+        <header className='header'>
+            <h2>Meme Generator</h2>
+            <h2>React Course Project</h2>
+        </header>
+        
+        
+    )
+}
