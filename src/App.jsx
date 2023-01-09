0df953ec12e2de19915c68bae5ecbea4161a7edd
emmacodes <binma.emma@gmail.com>
2023-01-09 15:52:21 -0800

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Header from './components/Header'
+import Meme from './components/Meme'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <Header />
+      <Meme />
  
     </div>
   )
